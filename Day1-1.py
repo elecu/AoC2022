e177f3d10faa6da88a6b81ea161ede5c37a98879
elecu <13957253+elecu@users.noreply.github.com>
2022-12-01 17:47:35 -0600

    Add files via upload

---
+++ Day1-1.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+
+
+total = 0
+x=0
+
+with open('/home/eherrera-chacon/Documents/Advent2022/Day1/input.txt') as infile:
+    with open('output.txt', 'w') as outfile:
+
+        for line in infile:
+            try:
+                num = int(line)
+                total += num
+                print(num, file=outfile)
+            except ValueError:
+                if total>x:
+                    x=total
+                    total=0
+                else:
+                    total=0
+   
+print(x)
