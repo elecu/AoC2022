d5de00997c3e882b4d661fddd55613829e445e94
elecu <13957253+elecu@users.noreply.github.com>
2022-12-01 22:52:47 -0600

    Add files via upload

---
+++ Day1-2.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+total = 0
+
+with open('/home/eherrera-chacon/Documents/Advent2022/Day1/input.txt') as infile:
+    with open('output2.txt', 'w') as outfile:
+
+        for line in infile:
+            try:
+                num = int(line)
+                total += num
+              
+            except ValueError:
+                print(total, file=outfile)
+                total = 0
+
+f = open("/home/eherrera-chacon/Documents/Advent2022/Day1/output2.txt", "r+")
+numbers = sorted(list(map(int, f.readlines())), reverse=True)
+print(numbers[:3])
+h = sum(numbers[:3])
+print(h)
